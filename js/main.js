8756097b0c5ec80205318df3cc3da8ab698d02c1
kaliraj <kalisdvk96@gmail.com>
2022-12-13 17:04:20 +0530

    area of a triangle

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let b=parseInt(prompt("Enter the base value of a triangle"));
+let h=parseInt(prompt("Enter the height value of a triangle"));
+let a=(b*h)/2;
+console.log("area of a triangle is "+a);
